c390981fa7c0e902ddd32b89cc00175455a067fe
18231550+reignreapsow@users.noreply.github.com <18231550+reignreapsow@users.noreply.github.com>
2021-01-08 14:57:31 -0500

    ADDED basic operators

---
+++ 01-Fundamentals-Part-1/starter/basic-operators.js
@@ -0,0 +1,19 @@
+//# basic operators
+
+console.log('//#########################//')
+console.log('//#### let, const, var ####')
+console.log('//#########################//')
+console.log('    ')// blank line
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah) //46 19
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); // 92 4.6 8
+// 2 ** 3 means 2 to the power of 3 = 2*2*2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + " " +lastName); //Jonas Schmedtmann
+
