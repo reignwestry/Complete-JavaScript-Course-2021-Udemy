90645c53d49088513f3b0665a439c3c2db4d3712
18231550+reignreapsow@users.noreply.github.com <18231550+reignreapsow@users.noreply.github.com>
2021-01-13 12:30:05 -0500

    CODE CHALLENGE 3 COMPLETED

--- 01-Fundamentals-Part-1/starter/coding-challenge-3.js
+++ 01-Fundamentals-Part-1/starter/coding-challenge-3.js
@@ -23,3 +23,58 @@ console.log("    "); // blank line
 * TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
 *
 * */
+
+//TEST1 === DOLPHINS WIN
+// const dolphins1 = 96;
+// const koalas1 = 88;
+//
+// const dolphins2 = 128;
+// const koalas2 = 87;
+//
+// const dolphins3 = 89;
+// const koalas3 = 110;
+
+//TEST2 === KOALAS WIN
+// const dolphins1 = 96;
+// const koalas1 = 88;
+//
+// const dolphins2 = 128;
+// const koalas2 = 87;
+//
+// const dolphins3 = 89;
+// const koalas3 = 110;
+
+//TEST 3 === DRAW
+
+const dolphins1 = 96;
+const koalas1 = 96;
+
+const dolphins2 = 87;
+const koalas2 = 87;
+
+const dolphins3 = 110;
+const koalas3 = 110;
+
+function getAvgScore (score1, score2, score3){
+   let avgScore = (score1 + score2 + score3) / 3;
+   return avgScore;
+}
+
+//# GET AVG SCORES
+let dolphinScore = getAvgScore(dolphins1, dolphins2, dolphins3);
+console.log(dolphinScore);
+
+let koalaScore = getAvgScore(koalas1, koalas2, koalas3);
+console.log(koalaScore);
+ //? 2. Compare the team's average scores to determine the winner of the competition, and print it to the console.
+   //?  Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+
+if (dolphinScore > koalaScore && dolphinScore >= 100 ) { //dolphins have a higher score
+   console.log(`Dolphins WIN! Dolphins ${dolphinScore} Koalas ${koalaScore}`);
+} else if (koalaScore > dolphinScore && koalaScore >= 100 ) {  //koalas have a higher score
+   console.log(`Koalas WIN! Dolphins ${dolphinScore} Koalas ${koalaScore}`);
+} else if ( koalaScore === dolphinScore || koalaScore && dolphinScore <= 100 ) { // same score or under 100 They
+   // have the same score
+   console.log(`IT WAS A DRAW! Dolphins ${dolphinScore} Koalas ${koalaScore}`);
+}
