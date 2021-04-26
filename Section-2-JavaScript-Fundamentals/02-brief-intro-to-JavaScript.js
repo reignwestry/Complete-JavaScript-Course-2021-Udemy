53ee7fec74e5aa38c16f1d83b722f167a2a6c683
Reign <reignzj.westry@gmail.com>
2021-04-26 01:08:54 -0400

    BEGAN OBJECTS INTRO

---
+++ Section-2-JavaScript-Fundamentals/02-brief-intro-to-JavaScript.js
@@ -0,0 +1,26 @@
+//# What is JavaScript?
+
+//? DEFINITION: JavaScript is a high-level, object-oriented, multi-paradigm programming language
+
+    //? Programming Language = instructs computer to do things
+
+    //? High-level = We don't have to worry about complex stuff like memory management
+
+    //? Object-Oriented = Based on objects, for storing most kinds of data
+
+    //? Multi-Paradigm = We can use different styles of programming 
+
+//# The Role of JavaScript in Web Development
+
+    //? HTML === Content --- NOUNS --- Data that is displayed
+    
+    //? CSS  === Presentation --- ADJECTIVES --- How content is presented
+
+    //? JS   === Programming Language: Building Web Applications --- VERBS --- ACTIONS --- What something does
+
+
+//# JavaScript Releases
+//?    ES6(ES2015) ----> ES11(ES2020) === MODERN JavaScript 
+
+//? ES === ECMAScript
+
---
+++ Section-2-JavaScript-Fundamentals/06-IntroToObjects.js
@@ -0,0 +1,12 @@
+// OBJECTS INTRO
+
+console.log('OBEJECTS INTRO') 
+console.log('##############') 
+
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Micheal', 'Peter', 'Steven']
+]
