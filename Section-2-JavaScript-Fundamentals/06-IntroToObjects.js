ae4d2428fdf9c1e8c4213d1ddab32b61c2936a2d
Reign <reignzj.westry@gmail.com>
2021-04-26 21:58:35 -0400

    continuing objects

--- Section-2-JavaScript-Fundamentals/06-IntroToObjects.js
+++ Section-2-JavaScript-Fundamentals/06-IntroToObjects.js
@@ -1,4 +1,5 @@
 // OBJECTS INTRO
+//#
 
 console.log('OBEJECTS INTRO') 
 console.log('##############') 
@@ -10,3 +11,17 @@ const jonasArray = [
     'teacher',
     ['Micheal', 'Peter', 'Steven']
 ]
+
+
+
+//Object
+const jonas = {
+
+        //Key value pairs
+    //# properties: values
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Micheal', 'Peter', 'Steven']
+}
