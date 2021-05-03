9f7a28afd8a1a5e4a0de799a1daeee2cec40ecfa
Reign <reignzj.westry@gmail.com>
2021-05-02 23:00:41 -0400

    ADDED FOR LOOP

---
+++ Section-2-JavaScript-Fundamentals/09-For-Loop.js
@@ -0,0 +1,22 @@
+console.log('####################################')
+console.log('#### For Loop ############')
+console.log('####################################')
+
+
+//# FOR LOOP
+//##########
+
+/*
+    + Initial value === starting point ex. 1 or 0
+    + logical condition === how it runs through iterations
+    + rep++ === adds 1 to rep after each iteration
+
+    for(initialValue; logical condition;how the loop counts){
+        What it does
+    }
+*/
+
+//RUNS 10 times
+for (let rep = 1; rep <= 10; rep++){
+    console.log(`Lifting weights repetition ${rep}🏋️‍♀️`)
+}
