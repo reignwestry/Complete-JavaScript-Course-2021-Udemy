d2a811817220db5d74945663265a933b523be7c8
Reign <reignzj.westry@gmail.com>
2022-07-09 09:14:15 -0400

    Starting automated git

---
+++ commit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd D:/.PROJECTS/.Udemy-Courses/.Udemy-JS-COURSE-2021/
+git add --all
+timestamp() {
+  date +"at %H:%M:%S on %d/%m/%Y"
+}
+git commit -am "Regular auto-commit $(timestamp)"
+git push origin master
